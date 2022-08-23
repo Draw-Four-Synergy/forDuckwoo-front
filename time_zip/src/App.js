02c103edddf99edd093375c82fd6706adde972bb
won0324 <huiwon0324@naver.com>
2022-08-24 06:04:40 +0900

    splash, 로그인 전 main 화면

--- time_zip/src/App.js
+++ time_zip/src/App.js
@@ -1,10 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Splash from "./components/Splash";
+import Home from "./components/Home";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      main
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Splash />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
