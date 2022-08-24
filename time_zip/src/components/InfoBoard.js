5c6b16bd8cb02b86a364011be304a65d80a02aeb
won0324 <huiwon0324@naver.com>
2022-08-24 19:54:38 +0900

    add navbar & mainCategory page

---
+++ time_zip/src/components/InfoBoard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const InfoBoard = () => {
+  return (
+    <div className="InfoBoard">
+      <Navbar />
+      info
+    </div>
+  );
+};
+
+export default InfoBoard;
