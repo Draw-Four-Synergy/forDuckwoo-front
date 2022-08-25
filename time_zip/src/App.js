525d6571bfc677b8c60ac0dbd41cbeebf1d0a8e0
seoyeon22 <hsy2709@gmail.com>
2022-08-26 02:01:39 +0900

    충돌

--- time_zip/src/App.js
+++ time_zip/src/App.js
@@ -6,7 +6,12 @@ import Account from "./components/Account";
 import MainCategory from "./components/MainCategory";
 import InfoBoard from "./components/InfoBoard";
 import Newsletter from "./components/Newsletter";
+import InfoWrite from "./components/InfoWrite";
 import MyPage from "./components/MyPage";
+import Englishnews from "./components/Englishnews";
+import English from "./components/English";
+import News from "./components/News";
+import Scrap from "./components/Scrap";
 
 const App = () => {
   return (
@@ -19,7 +24,12 @@ const App = () => {
           <Route path="/main" element={<MainCategory />}></Route>
           <Route path="/info" element={<InfoBoard />}></Route>
           <Route path="/newsletter" element={<Newsletter />}></Route>
+          <Route path="/infowrite" element={<InfoWrite />}></Route>
           <Route path="/my_page" element={<MyPage />}></Route>
+          <Route path="/english_news" element={<Englishnews />}></Route>
+          <Route path="/news" element={<News/>}></Route>
+          <Route path="/english" element={<English />}></Route>
+          <Route path="/scrap" element={<Scrap />}></Route>
         </Routes>
       </Router>
     </>
@@ -27,3 +37,4 @@ const App = () => {
 };
 
 export default App;
+
