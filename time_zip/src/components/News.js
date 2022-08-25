432f0e302c99b869091c4233af95eeabaede81a3
seoyeon22 <hsy2709@gmail.com>
2022-08-26 06:27:24 +0900

    modify newslette

--- time_zip/src/components/News.js
+++ time_zip/src/components/News.js
@@ -10,111 +10,85 @@ import { useLocation } from "react-router-dom";
 import axios from "axios";
 
 function News() {
-  /*const audiobook_emo = () => {
-        const[item, setItem] = useState(0)
-        const incrementItem = () => setItem(item+1)
-    }*/
-  const jwt = localStorage.getItem("jwt");
-  const location = useLocation();
-  const category = location.state.category;
-  const id = location.state.id;
-  console.log(id);
+    /*const audiobook_emo = () => {
+            const[item, setItem] = useState(0)
+            const incrementItem = () => setItem(item+1)
+        }*/
+    const jwt = localStorage.getItem("jwt");
+    const location = useLocation();
+    const category = location.state.category;
+    const id = location.state.id;
 
-  let [like, setLike] = useState(0);
-  let [emoName, setEmoName] = useState(["좋아요", "슬퍼요", "화나요"]);
+    const [news, setNews] = useState([]);
+    const [emoticon, SetEmoticon] = useState([]);
 
-  const [news, setNews] = useState([]);
+    const getInfo = async () => {
+        const response = await axios.get(
+        `http://3.36.252.208:9000/contents/${category}/${id}`,
+        {
+            headers: {
+            "x-access-token": jwt,
+            },
+        }
+        );
+        if (response.data.isSuccess === true) {
+        setNews(response.data.result);
+        }
+    };
 
-  const getInfo = async () => {
-    const response = await axios.get(
-      `http://3.36.252.208:9000/contents/${category}/${id}`,
-      {
-        headers: {
-          "x-access-token": jwt,
-        },
-      }
+    const onClick = () => {
+        const current = document.getElementById("scrap-btn");
+        current.style.backgroundColor = "#FFDD2A";
+    };
+
+    useEffect(() => {
+        getInfo();
+    }, []);
+
+    return (
+        <div className="englishnews">
+            <Navbar />
+            <h1>News</h1>
+            <div className="englishnews_title">
+                <p>{news.title}</p>
+            </div>
+
+            <div className="englishnews_page">
+                <iframe className="news_content" src={news.content}></iframe>
+            </div>
+
+            <div className="englishnews_emoscrap">
+                <span className="englishnews_emo">
+                    <div className="englishnews_smile">
+                        <img src={smile} alt="smile" width="50px" height="50px"></img>
+                        <p className="emoticon-num">{news && news.smile}</p>
+                    </div>
+
+                    <div className="englishnews_sad">
+                        <img src={sad} alt="sad" width="50px" height="50px"></img>
+                        <p className="emoticon-num">{news && news.cry}</p>
+                    </div>
+
+                    <div className="englishnews_angry">
+                        <img src={angry} alt="angry" width="60px" height="50px"></img>
+                        <p className="emoticon-num">{news && news.angry}</p>
+                    </div>
+                </span>
+
+                <span className="englishnews_scrap">
+                    <button className="scrap-btn" id="scrap-btn" onClick={onClick}>
+                        <img src={scrap} alt="scrap" width="15%" height="15%"></img>스크랩
+                    </button>
+                </span>
+            </div>
+
+            <div className="englishnews_chart">
+                <Link to="/newsletter">
+                <button className="chart">목록</button>
+                </Link>
+            </div>
+        </div>
     );
-    console.log(response.data);
-    if (response.data.isSuccess === true) {
-      setNews(response.data.result);
-    }
-  };
-
-  useEffect(() => {
-    getInfo();
-  }, []);
-
-  return (
-    <div className="englishnews">
-      <Navbar />
-      <h1>News</h1>
-      <div className="englishnews_title">
-        <p>{news.title}</p>
-      </div>
-
-      <div className="englishnews_page">
-        <iframe className="news_content" src={news.content}></iframe>
-      </div>
-
-      <div className="englishnews_emoscrap">
-        <span className="englishnews_emo">
-          <span className="englishnews_smile">
-            {" "}
-            <img src={smile} alt="smile" width="30" height="30"></img>
-            <p>좋아요</p>
-            <span
-              onClick={() => {
-                setLike(like + 1);
-              }}
-            >
-              좋아요
-            </span>
-            {like}{" "}
-          </span>
-
-          <span className="englishnews_sad">
-            {" "}
-            <img src={sad} alt="sad" width="30" height="30"></img>
-            <p>슬퍼요</p>
-            <span
-              onClick={() => {
-                setLike(like + 1);
-              }}
-            >
-              슬퍼요
-            </span>
-            {like}{" "}
-          </span>
-
-          <span className="englishnews_angry">
-            {" "}
-            <img src={angry} alt="angry" width="30" height="30"></img>
-            <p>화나요</p>
-            <span
-              onClick={() => {
-                setLike(like + 1);
-              }}
-            >
-              화나요
-            </span>
-            {like}{" "}
-          </span>
-        </span>
-
-        <span className="englishnews_scrap">
-          <img src={scrap} alt="scrap" width="30" height="30"></img>
-          <p>스크랩</p>
-          <p>스크랩</p>
-        </span>
-      </div>
-
-      <div className="englishnews_chart">
-        <Link to="/newsletter">
-          <button className="chart">목록</button>
-        </Link>
-      </div>
-    </div>
-  );
 }
 
 export default News;
